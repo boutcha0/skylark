362e197912880135c2c84888d7ee6142789deb20
bamboutcho <hamzaoukhouyaxx@gmail.com>
2024-12-13 14:50:50 +0100

    StripeAPI in process

---
+++ src/main/java/com/crudops/skylark/config/StripeConfig.java
@@ -0,0 +1,2 @@
+package com.crudops.skylark.config;public class StripeConfig {
+}
---
+++ src/main/java/com/crudops/skylark/controller/PaymentRestController.java
@@ -0,0 +1,31 @@
+package com.crudops.skylark.controller;
+
+import com.crudops.skylark.DTO.PaymentRequest;
+import com.crudops.skylark.service.PaymentsService;
+import com.crudops.skylark.service.impl.PaymentService;
+import com.stripe.exception.StripeException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/payments")
+public class PaymentRestController {
+    private final PaymentService paymentService;
+
+    public PaymentRestController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping("/create-payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentRequest request) {
+        try {
+            String clientSecret = paymentService.createPaymentIntent(request);
+            return ResponseEntity.ok(clientSecret);
+        } catch (StripeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
